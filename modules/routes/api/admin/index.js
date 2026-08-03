6ab8bdc87eed498d56d405fa6f03d0152445b41d
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2026-08-03 22:19:21 +0330

    add auth middleware on admin routes

--- modules/routes/api/admin/index.js
+++ modules/routes/api/admin/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const adminRouter = express.Router();
 
 //courses routes
-
 const coursesRouter = require("./courses");
 const episodesRouter = require("./episodes");
 
-adminRouter.use("/admin", coursesRouter);
-adminRouter.use("/admin", episodesRouter);
+//middlewares
+
+const authMiddleware = require(`${global.config.path.middlewares}/authenticate`);
+
+adminRouter.use("/admin", authMiddleware, coursesRouter);
+adminRouter.use("/admin", authMiddleware, episodesRouter);
 
 module.exports = adminRouter;
