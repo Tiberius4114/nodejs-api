dffaf50d477eb53133dd3b7f84e261916fddb172
mohammadZahedii <mohammadzahedi_1995@yahoo.com>
2026-08-30 03:02:35 +0330

    change CourseTransform methods for showing episodes

--- modules/transforms/v1/course/index.js
+++ modules/transforms/v1/course/index.js
@@ -2,11 +2,27 @@ const Transform = require("../../transform");
 
 class CourseTransform extends Transform {
   transform(item) {
+    console.log(this, "THIS");
     return {
       title: item.title,
       body: item.body,
       price: item.price,
+      ...this.showEpisodes(item),
     };
+  }
+
+  showEpisodes(item) {
+    if (this.withEpisodesStatus) {
+      return {
+        episodes: item.episodes,
+      };
+    }
+    return {};
+  }
+  withEpisodes() {
+    this.withEpisodesStatus = true;
+    // console.log(this, "THIS");
+    return this;
   }
 }
 
